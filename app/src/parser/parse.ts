657ca6db757b189f44cfc80a9a7cd65b9c3dd39c
Jesse Hoogland <jessequinten@gmail.com>
2022-07-10 21:57:00 +0200

    RM .vscode from tracking

---
+++ app/src/parser/parse.ts
@@ -0,0 +1,3 @@
+/**
+ * Eventually offload this into `rewiki`, but for now it's enough to parse the templates & append section headings
+ */
--- app/src/server/router/wiki.ts
+++ app/src/server/router/wiki.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 import { createRouter } from "./context";
 
+const EntryRequestSchema = z.object({
+  word: z.string().min(1),
+  lang: z.string().default("en"),
+  wiki: z.string().default("en"),
+});
 interface WikiResponse {
   parse: {
     title: string;
@@ -11,29 +16,46 @@ interface WikiResponse {
   };
 }
 
-const wikiRouter = createRouter().query("getWikitext", {
-  meta: {
-    openapi: {
-      enabled: true,
-      method: "GET",
-      path: "/wiki/text/{word}",
-      summary: "Read a result from the wiktionary api",
-      description: "Retrieve the wiktionary results for the given id",
+const getWikiText = async (input: z.infer<typeof EntryRequestSchema>) =>
+  fetch(
+    `https://${input?.wiki}.wiktionary.org/w/api.php?action=parse&page=${input.word}&format=json&prop=wikitext`
+  )
+    .then((res) => res.json())
+    .then((res: WikiResponse) => res.parse.wikitext["*"]);
+
+const wikiRouter = createRouter()
+  .query("getWikiText", {
+    meta: {
+      openapi: {
+        enabled: true,
+        method: "GET",
+        path: "/wiki/text/{word}",
+        summary: "Get Wikitext",
+        description: "Retrieve the wiktionary results for the given id",
+      },
     },
-  },
-  input: z.object({
-    word: z.string().min(1),
-    lang: z.string().default("en"),
-    wiki: z.string().default("en"),
-  }),
-  output: z.string(),
-  async resolve({ input }) {
-    return fetch(
-      `https://${input?.wiki}.wiktionary.org/w/api.php?action=parse&page=${input.word}&format=json&prop=wikitext`
-    )
-      .then((res) => res.json())
-      .then((res: WikiResponse) => res.parse.wikitext["*"]);
-  },
-});
+    input: EntryRequestSchema,
+    output: z.string(),
+    async resolve({ input }) {
+      return getWikiText(input);
+    },
+  })
+  .query("getWikiJson", {
+    meta: {
+      openapi: {
+        enabled: true,
+        method: "GET",
+        path: "/wiki/ld/{word}",
+        summary: "Get entry JSON-LD",
+        description:
+          "Parse the linked data contained on the corresponding wiktionary page",
+      },
+    },
+    input: EntryRequestSchema,
+    output: z.string(),
+    async resolve({ input }) {
+      return getWikiText(input).then(parseWikitext);
+    },
+  });
 
 export default wikiRouter;
